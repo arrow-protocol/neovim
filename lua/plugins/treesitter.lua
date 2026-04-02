0095277f05e983d03a8d437be8eb8e2560232ba3
zhantemir <k.zhantemir@documentolog.com>
2026-04-02 17:30:01 +0500

    fix(treesitter): migrate to new opts API after nvim-treesitter removed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,11 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {"javascript", "typescript", "tsx", "json", "html", "css", "lua"},
-            auto_install = true,
-            highlight = {
-                enable = true
-            }
-        })
-    end
+    opts = {
+        ensure_installed = {"javascript", "typescript", "tsx", "json", "html", "css", "lua"},
+        auto_install = true,
+        highlight = {
+            enable = true
+        }
+    }
 }
