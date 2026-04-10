bf2b90dc30b9ffe4849c86bd42815548f5674fe2
rogue <zhantemir.k.e@gmail.com>
2026-04-11 01:54:46 +0500

    feat: add options for integrations in catppuccin plugin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,6 +2,11 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
+	opts = {
+		integrations = {
+			mason = true,
+		},
+	},
 	config = function()
 		vim.cmd.colorscheme("catppuccin")
 	end,
