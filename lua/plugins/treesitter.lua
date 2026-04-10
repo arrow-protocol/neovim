70089b2e6c3ae463cacc8129787aba66de56e4cc
rogue <zhantemir.k.e@gmail.com>
2026-04-11 01:25:16 +0500

    style: normalize indentation to tabs across plugin configs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-        ensure_installed = {"javascript", "typescript", "tsx", "json", "html", "css", "lua"},
-        auto_install = true,
-        highlight = {
-            enable = true
-        }
-    }
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	opts = {
+		ensure_installed = { "javascript", "typescript", "tsx", "json", "html", "css", "lua" },
+		auto_install = true,
+		highlight = {
+			enable = true,
+		},
+	},
 }
