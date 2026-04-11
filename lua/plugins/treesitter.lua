998e17c60d848dc9afca98f68584c4954604c6e0
rogue <zhantemir.k.e@gmail.com>
2026-04-11 14:54:50 +0500

    feat: add stylua formatter for lua files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	opts = {
-		ensure_installed = { "javascript", "typescript", "tsx", "json", "html", "css", "lua" },
+		ensure_installed = { "javascript", "typescript", "jsx", "tsx", "json", "html", "css", "lua" },
 		auto_install = true,
 		highlight = {
 			enable = true,
