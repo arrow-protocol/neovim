1b57e22a84c9b83a2cae49c8b3bdd8fdf5afb476
rogue <zhantemir.k.e@gmail.com>
2026-04-11 14:55:48 +0500

    feat: add stylua formatter for lua files

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -4,6 +4,7 @@ return {
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
+				lua = { "stylua" },
 				javascript = { "prettierd" },
 				javascriptreact = { "prettierd" },
 				typescript = { "prettierd" },
