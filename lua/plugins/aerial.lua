1e0ce8cb1fa9fb1966e53da7b4d0366e9dcac03e
zhantemir <k.zhantemir@documentolog.com>
2026-04-14 15:16:51 +0500

    feat: add aerial

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -1,4 +1,22 @@
 return {
 	"stevearc/aerial.nvim",
-	opts = {},
+	opts = {
+		layout = {
+			max_width = { 80, 0.2 },
+			min_width = 40,
+			default_direction = "left",
+		},
+		nerd_font = true,
+
+		on_attach = function(bufnr)
+			vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+			vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+		end,
+
+		show_guides = true,
+	},
+	keys = {
+		{ "<leader>a", "<cmd>AerialToggle!<CR>", desc = "Toggle Aerial" },
+		{ "<leader>o", "<cmd>AerialNavToggle<CR>", desc = "Aerial Nav" },
+	},
 }
